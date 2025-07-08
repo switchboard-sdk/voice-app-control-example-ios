c56a9daeb77c2a80383bf3c4921f28da244c49d2
TayyabJaved <engr.tj@gmail.com>
2025-07-08 01:31:26 -0400

    Create setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eu
+
+PROJECT_DIR="$(git rev-parse --show-toplevel)"
+PLATFORM="ios"
+SDK_VERSION="release/3.0.0-beta"
+
+PACKAGE_NAMES=(
+    "SwitchboardSDK"
+    "SwitchboardOnnx"
+    "SwitchboardSileroVAD"
+    "SwitchboardWhisper"
+)
+
+for PACKAGE_NAME in "${PACKAGE_NAMES[@]}"; do
+
+    LIBS_DIR="${PROJECT_DIR}/Frameworks/${PACKAGE_NAME}/${PLATFORM}"
+    rm -rf "${LIBS_DIR}"
+    mkdir -p "${LIBS_DIR}"
+
+    TMP_LIBS_DIR="${LIBS_DIR}/tmp"
+    mkdir -p "${TMP_LIBS_DIR}"
+
+    pushd "${TMP_LIBS_DIR}"
+
+    echo "Downloading ${PACKAGE_NAME}..."
+    curl -O "https://switchboard-sdk-public.s3.amazonaws.com/builds/${SDK_VERSION}/${PLATFORM}/${PACKAGE_NAME}.zip"
+
+    echo "Unpacking ${PACKAGE_NAME}..."
+    mkdir -p "${LIBS_DIR}"
+    unzip "${PACKAGE_NAME}.zip" -d "${LIBS_DIR}"
+    
+    popd
+    
+    rm -rf "${TMP_LIBS_DIR}"
+done
